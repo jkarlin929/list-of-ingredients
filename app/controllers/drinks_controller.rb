79b5178979f06f4cba8d23652c202acd32958bea
jkarlin929 <osida11572@gmail.com>
2019-03-30 20:01:17 -0400

    add procfile

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -3,14 +3,16 @@ class DrinksController < ApplicationController
 
   # GET /drinks
   def index
-    @drinks = Drink.all
+    @drinks = Drink.select("id, title").all
 
     render json: @drinks
   end
 
   # GET /drinks/1
   def show
-    render json: @drink
+    @drink = Drink.find(params[:id])
+
+    render json: @drink.to_json(:include => {:ingredients => {:only => [:id, :description] }})
   end
 
   # POST /drinks
